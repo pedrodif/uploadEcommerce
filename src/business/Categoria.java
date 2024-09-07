d087eb7bc474c8ab9a77e31d60f25cff1fc99bf6
Pedro Ferreira <pedrodif@icloud.com>
2024-09-07 15:21:37 -0300

    feat: :sparkles: implementa classe Categoria

---
+++ src/business/Categoria.java
@@ -0,0 +1,33 @@
+package business;
+
+import java.util.ArrayList;
+
+public class Categoria {
+    private int codCategoria;
+    private ArrayList<Produto> produtos;
+    private String nomeCategoria, departamento;
+
+    public Categoria(int codCategoria, String nomeCategoria, String departamento) {
+        this.codCategoria = codCategoria;
+        this.departamento = departamento;
+        this.nomeCategoria = nomeCategoria;
+        this.produtos = new ArrayList<Produto>();
+    }
+
+    public int getCodCategoria() {
+        return codCategoria;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public boolean addProduto(Produto produto) {
+        return this.produtos.add(produto);
+    }
+
+}
